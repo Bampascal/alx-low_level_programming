de0251c74d3ec93ed41e7c2aac50e69238830be4
Bampascal <akinolabamemmanuel@gmail.com>
2022-10-13 19:18:58 -0700

    alx-low_level_programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
-*main - Entry point*
+*main - Entry point
+*
 * Return: Always 0 (Success)
 *
 */
 int main(void)
-{int the_int;
+{	int the_int;
 	float the_float;
 	char the_char;
 	long long the_long_long;
