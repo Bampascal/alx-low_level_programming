2908682280505b09ec1f3429f4fb619e8d412323
Bampascal <akinolabamemmanuel@gmail.com>
2022-10-14 11:59:20 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point 2-print_alphabet.c
+ *
+ * Return: Always 0 (Successful 2-print_alphabet.c)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
