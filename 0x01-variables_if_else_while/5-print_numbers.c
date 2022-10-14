7b4535f404ca82fb02f6d17278665fe48678f465
Bampascal <akinolabamemmanuel@gmail.com>
2022-10-14 12:32:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry of point 5-print_numbers.c
+ * Return: Always return 0 (succeful source code of 5-print_numbers.c)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n" , i);
+	}
+	return (0);
+}
