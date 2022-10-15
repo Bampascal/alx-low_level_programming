f606f085ec91372c8a0accd3db791958685f9b3a
Bampascal <akinolabamemmanuel@gmail.com>
2022-10-15 13:25:25 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,6 @@ int main(void)
 		for (num2 = 0; num2 < 10; num2++)
 		{
 			if (num1 != num2 && num1 < num2)
-				continue;
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
 				if (num1 == 9 && num2 == 8)
