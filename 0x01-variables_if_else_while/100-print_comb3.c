4556472c4164053177c3984543404c3e0e070505
Bampascal <akinolabamemmanuel@gmail.com>
2022-10-15 14:02:03 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,14 @@ int main(void)
 	for (num1 = 0; num1 < 10; num1++)
 		for (num2 = 0; num2 < 10; num2++)
 		{
-			if (num1 != num2 && num1 < num2)
-			{
+			if (num1 == num2 || num2 < num1)
+				continue;
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
-				if (num1 == 9 && num2 == 8)	
-					continue;
+				if (num1 == 8 && num2 == 9)	
+					break;
 				putchar(',');
 				putchar(' ');
-			}
 		}
 	putchar('\n');
 	return (0);
