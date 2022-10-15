eb02812e67c7521bef9a2d463e68474aca52d5db
Bampascal <akinolabamemmanuel@gmail.com>
2022-10-14 23:24:12 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return:0 for the 8-print_base16.c
+ */
+int main(void)
+{
+	char i;
+	int num;
+
+	for (i = 'a'; i <= 'z'; i++);
+	{
+		putchar(i);
+	}
+	for (num = 0; num < 10; num++);
+	{
+		putchar((num % 10) + '0');
+	}
+	putchat('\n');
+	return (0);
+}
