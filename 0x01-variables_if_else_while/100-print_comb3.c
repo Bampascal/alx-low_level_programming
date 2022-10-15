5caf6664771b4254808004fda11c159938b6d8e6
Bampascal <akinolabamemmanuel@gmail.com>
2022-10-15 15:01:08 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 /**
-* main - BamPascal to prints all possible different combinations of two digits.
-* Return:0
-*/
+ * main - Bampascal to print all possible different combinations of two digits.
+ * Return:0
+ */
 int main(void)
 {
 	int num1, num2;
-
-	for (num1 = 0; num1 < 10; num1++)
-		for (num2 = 0; num2 < 10; num2++)
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			if (num1 == num2 || num2 < num1)
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 8 && num2 == 9)
 				continue;
-				putchar((num1 % 10) + '0');
-				putchar((num2 % 10) + '0');
-				if (num1 == 8 && num2 == 9)	
-					break;
-				putchar(',');
-				putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
-	putchar('\n');
+	}
+	putchar('\n')
 	return (0);
 }
