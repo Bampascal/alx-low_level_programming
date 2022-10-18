b9fe9ef6f4bf91d73a83741e567d377810bdcbf3
Bampascal <akinolabamemmanuel@gmail.com>
2022-10-18 13:46:40 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main BamPascal 0-putchar.c
+ * Return:0
+ */
+int main(void)
+{
+	_putchar('B');
+	_putchar('A');
+	_putchar('M');
+	_putchar('P');
+	_putchar('A');
+	_putchar('S');
+	_putchar('C');
+	_putchar('A');
+	_putchar('L');
+	_putchar('\n');
+}
