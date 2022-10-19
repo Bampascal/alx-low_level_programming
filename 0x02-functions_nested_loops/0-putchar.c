4fa14d1bf4599a883423e7ae5978227adef09a8c
Bampascal <akinolabamemmanuel@gmail.com>
2022-10-19 03:40:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - print putchar
- * Return: 0
+ * Return:0
  */
 int main(void)
 {
@@ -14,5 +14,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0); 
 }
