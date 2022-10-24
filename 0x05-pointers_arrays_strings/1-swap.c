5d3d789a18824b772fd4523b7a724fae29a1bb49
Bampascal <akinolabamemmanuel.com>
2022-10-24 05:21:22 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int(int *a, int *b)
+ *
+ * @a: An integer to swap
+ * @b: Another interger to swap
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int num;
+
+	num = *a;
+	*a = *b;
+	*b = num;
+}
+
