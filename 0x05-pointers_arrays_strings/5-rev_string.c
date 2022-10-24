9a43d695c7ed9d828b696148333d4779866be587
Bampascal <akinolabamemmanuel.com>
2022-10-24 09:13:33 -0700

    5-rev_string.c Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,10 +3,10 @@
 
 /**
  * rev_string - Reverses a string
-  * @s: The string to be modified
-  *
-  * Return: void
-  */
+ * @s: The string to be modified
+ *
+ * Return: void
+ */
 void rev_string(char *s)
 {
 	
