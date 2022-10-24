7d36ab9ecfebfc319f9a52b1109038644ba3fae7
Bampascal <akinolabamemmanuel.com>
2022-10-24 05:29:04 -0700

    1-swap.c Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int(int *a, int *b) - Swap two integer pointers
+ * swap_int - Swap two integer pointers
  *
  * @a: An integer to swap
  * @b: Another interger to swap
