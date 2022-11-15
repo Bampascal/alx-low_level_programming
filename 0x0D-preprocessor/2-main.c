f2480d75510b34b049b4afa563b0ee213697c225
Bampascal <akinolabamemmanuel.com>
2022-11-15 13:03:27 -0800

    2-main.c task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - prints the name of file program was complied from
+ * main - prints the name of the file program was compiled from
  * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
-	printf("%s\n", _FILE_);
-
+	printf("%s\n", __FILE__);
 	return (0);
 }
