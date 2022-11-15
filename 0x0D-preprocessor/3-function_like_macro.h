6bf82e4725036f0589929a70d318335955e21175
Bampascal <akinolabamemmanuel.com>
2022-11-15 13:41:59 -0800

    3-function_like_macro.h task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _HEADER_H
-#define _HEADER_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? (x) * (-1) : (x))
 
 #endif
